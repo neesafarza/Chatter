c411a75f05ad5d6a5ab35d2b7f70025ee5b0bba0
Aneesa Farzana <aneesafarzana@Aneesas-MacBook-Air.local>
2020-04-14 18:16:06 +0100

    adding messages

---
+++ planning.md
@@ -0,0 +1,22 @@
+#Goal
+
+- Build a simple chat application using HTML, CSS, JavaScript and jQuery. 
+- It’s a single page, that opens in the browser, 
+- and shows the messages in colored bubbles (think something like WhatsApp). 
+- On one side the computer will generate a random conversation, 
+- on the other side the user can post messages. 
+- The screen automatically flows and follows the messages posted, like in all chat apps. 
+- Each message should also have a snall indication of the time it was sent.
+
+1. Display box to send message
+2. Display box to see message
+3. Click button to start conversation
+4. Computer start generating questions
+5. User answer will be taken as a value
+6. And return to the screen
+    - upon pressing enter display next question by computer
+7. Display time next to sent message.
+8. keep the questions and answer on display
+9. Computer sent next questions.
+10. 'q' to quit. 
+11. Or if it comes to the end, say goodbye and quit program
--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+
+
 $(document).ready(()=> {
 
-    $('#ask-button').click(() => {
-        $('#question-area').text(()=>{
-            return convo.getQuestion()
-        })
+   
+    $('#question-area').text(()=>{
+        return convo.getQuestion()
     })
+  
   $('#ans-button').click(() => {
     //   get value from text area
     let answer = $('#input-area').val()
-    $('#answer-area').text(() => answer)
+    $('#messages').append(() => `<div class='answer'>${answer} <h3 class='time' align='right'>${time}</h3></div> `)
+    
+    $('#messages').append(()=>{
+        return `<div class='question'>${convo.getQuestion()}<h3 class='time'>${time}</h3> </div>`
+    })
+    $('#input-area').val(() => {
+        return '';
+    })
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
   }) 
+
+  $('#input-area').keypress(function (e) {
+    var key = e.which;
+    if(key === 13){
+        $('#ans-button').click();
+        return false;
+    }
+  }
+  )
 })
+
